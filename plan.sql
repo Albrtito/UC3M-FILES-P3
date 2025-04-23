f4ce461aee7fe8bdf2da874800ae4d15e2b1dea4
tito <alb.pascau@gmail.com>
2025-04-23 17:12:16 +0200

    add some sql from presentations

--- plan.sql
+++ plan.sql
@@ -1,1 +1,6 @@
 explain plan set statement_id = 'myplan_1' for select * from editions where pub_place='Madrid';
+
+
+--select * from table(DBMS_XPLAN.DISPLAY);
+--SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY(NULL, 'myplan_1', 'BASIC'));
+--SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY(FORMAT=>'+ALLSTATS'));
